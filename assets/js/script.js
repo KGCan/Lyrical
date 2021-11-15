2da891be748df23faf8b1382568e586863d959b6
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-11-14 19:26:41 -0800

    added initial fetch request for ovh api

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+var buttonSearch = document.querySelector(".searchBtn")
+buttonSearch.addEventListener("click", function(){
+    var searchText = document.querySelector(".searchInput")
+    var LyricsGoHere = document.getElementById("appendHere")
+    console.log(searchText.value)
+    fetch("https://api.lyrics.ovh/v1/sia/"+searchText.value)
+    .then(response => response.json())
+    .then(song => {
+        console.log(song.lyrics)
+        LyricsGoHere.append(song.lyrics)
+    })
+})
