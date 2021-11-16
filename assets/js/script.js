9aa6ed91f2c89e7f0260812a8c1d5b2fa0f79ad5
Fatemeh Brizendine <ssaberi324@yahoo.com>
2021-11-15 16:08:54 -0800

    add deezer api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,48 @@
 var buttonSearch = document.querySelector(".search-button")
-
+var trackList; 
+  
 buttonSearch.addEventListener("click", function(){
     var searchText = document.querySelector(".input-group-field")
-    var LyricsGoHere = document.getElementById("results")
+    var results = document.getElementById("results")
+    var artistUrl = "https://cors-anywhere.herokuapp.com/api.deezer.com/artist/"+ searchText.value
     console.log(searchText.value)
-    fetch("https://api.lyrics.ovh/v1/sia/"+searchText.value)
+    function getArtist() {
+        fetch(artistUrl, {
+        mode: 'cors',
+        headers: {
+            'Access-Control-Allow-Origin':'*'
+          }
+        })
+        .then(response => response.json())
+        .then(song => {
+            console.log(song.tracklist)
+            trackList = song.tracklist
+            return song.tracklist
+    })
+    }
+    console.log(trackList)
+    fetch(trackList)
+    // console.log(getArtist())
     .then(response => response.json())
-    .then(song => {
-        console.log(song.lyrics)
-        LyricsGoHere.append(song.lyrics)
+    .then(tracks => {
+        console.log(tracks)
+        results.append(song.tracklist)
     })
 })
+
+// buttonSearch.addEventListener("click", function(){
+//     var searchText = document.querySelector(".input-group-field")
+//     var LyricsGoHere = document.getElementById("results")
+//     var lyricsUrl = "https://api.lyrics.ovh/v1/sia/"+searchText.value
+//     console.log(searchText.value)
+//     fetch(lyricsUrl)
+//     .then(response => response.json())
+//     .then(song => {
+//         console.log(song.lyrics)
+//         LyricsGoHere.append(song.lyrics)
+//     })
+// })
+
 var searchLyrics = function (){
   //get search res 
     var url = "https://api.lyrics.ovh/v1/"+ artist + "/" + song
