b1b862aca4ac2386748d7c84bef9d6e218ee10eb
Jazzmin S. Goodman <js.goodman@Jazzmins-MBP.attlocal.net>
2021-11-16 20:07:18 -0800

    initial setup fetch request for backup API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
+var lastKey = "6711c7f59db994bf3d0a996730f71a66"
+
 var buttonSearch = document.querySelector(".searchBtn")
+
+buttonSearch.addEventListener("click", function(){
+    var searchText = document.querySelector(".searchInput")
+    var LyricsGoHere = document.getElementById("appendHere")
+    console.log(searchText.value)
+    fetch("http://www.last.fm/api/auth/?api_key=" + lastKey 
+    .then(response => response.json())
+    .then(song => {
+        console.log(song.lyrics)
+        LyricsGoHere.append(song.lyrics)
+    })
+    )})
+
+/*var buttonSearch = document.querySelector(".searchBtn")
 
 buttonSearch.addEventListener("click", function(){
     var searchText = document.querySelector(".searchInput")
@@ -10,4 +26,4 @@ buttonSearch.addEventListener("click", function(){
         console.log(song.lyrics)
         LyricsGoHere.append(song.lyrics)
     })
-})
+})*/
